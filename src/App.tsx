f75a56023bffae6bae6bce8fd7d399e6f9b97d6d
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-02-09 21:02:45 +0500

    feat(chat): identify user

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { User } from "./types";
 
 function App() {
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const name = prompt("What is your name?");
+
+        setUser({ id: new Date().getTime(), name: name! });
+    }, []);
+
     return <div className="App">Home</div>;
 }
 
