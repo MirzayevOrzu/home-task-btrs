8eed7b8fc61a6b8c3613829912c623a3c3a68637
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-02-09 21:32:25 +0500

    feat(chat): initialize storage for messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { UserProfile } from "./components/UserProfile";
+import { defaultMessages } from "./fixtures/messages";
 import { User } from "./types";
 
 function App() {
@@ -7,6 +8,14 @@ function App() {
 
     useEffect(() => {
         const name = prompt("What is your name?");
+
+        const messagesJSON = localStorage.getItem("messages");
+
+        if (!messagesJSON) {
+            localStorage.setItem("messages", JSON.stringify(defaultMessages));
+        } else {
+            // TODO save to local state (redux, react state)
+        }
 
         setUser({ id: new Date().getTime(), name: name! });
     }, []);
